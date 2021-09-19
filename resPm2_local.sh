e3feb04000f3bac83a649e63db1e2b6bba125654
Matt Macdonald <mksmacdonald@gmail.com>
2021-09-19 16:20:52 -0700

    scripts to start a reload server

--- resPm2_local.sh
+++ resPm2_local.sh
@@ -1,9 +1,9 @@
 
 # Starts and reinitializes software and loads logs
 
-pm2 stop index
+pm2 stop "planet-snake-hardware-api"
 pm2 flush
-pm2 start index.js
+pm2 start index.js --name "planet-snake-hardware-api"
 pm2 logs --raw
 
 
--- resPm2_remote.sh
+++ resPm2_remote.sh
@@ -1,7 +1,8 @@
 
 # Starts and reinitializes software and loads logs
 
-pm2 stop --name "planet-snake-hardware-api"
+pm2 stop "planet-snake-hardware-api"
+pm2 remove "planet-snake-hardware-api"
 git pull
 pm2 flush
 pm2 start index.js --name "planet-snake-hardware-api"
