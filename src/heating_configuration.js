f11e0b50dda8b6714c5abbe0312a8e87d9ef0a61
Matt Macdonald <mksmacdonald@gmail.com>
2021-09-19 16:33:23 -0700

    removed magic numbers and capitalized constants

--- src/heating_configuration.js
+++ src/heating_configuration.js
@@ -1,55 +1,57 @@
 const ds18b20 = require("./sensors/ds18b20");
-const heatingControl = require("./control_modules/onOff_vivariumHeating");
-const humidityControl = require("./control_modules/onOff_vivariumHumidity");
+const heatingControl = require("./control_modules/heating");
+const humidityControl = require("./control_modules/humidity");
 
+// These are the addresses for the current sensors on the 1 wire Bus
 const hideSensor = "28-0000058f5428";
 const baskingSensor = "28-041464652fff";
 const coolSide = "28-0214630eb5ff";
 
 const temperatureSensors = ds18b20(60 * 1000, 5);
 
-const sensors = [hideSensor, baskingSensor, coolSide, "28-0000058fc62a"];
+const lcd = require("./display/lcd");
+const LCD_ADDRESS = 0x27;
+const lcdDisplay = lcd(LCD_ADDRESS);
 
+const SECOND_IN_MS = 1000;
+const SENSOR_POLLING_INTERVAL = 60 * SECOND_IN_MS;
+
+const humidityModule = require("./sensors/sht31_sensorControl");
+const humiditySensor = humidityModule(60 * SECOND_IN_MS, lcdDisplay, 3);
+
+const TARGET_RELATIVE_HUMIDITY = 65;
 const TARGET_HIDE_TEMPERATURE = 32;
 const TARGET_BASKING_TEMP = 35;
 
-const lcd = require("./display/lcd");
-const lcd_add = 0x27;
-const lcd_display = lcd(lcd_add);
-const seconds = 1000;
-
-const humidityModule = require("./sensors/sht31_sensorControl");
-const humiditySensor = humidityModule(60 * seconds, lcd_display, 3);
-
 const mister = humidityControl(
-  [1],
+  [1], // Powerbar Socket to register
   humiditySensor,
-  65,
-  lcd_display,
-  3,
-  5 * seconds
+  TARGET_RELATIVE_HUMIDITY,
+  lcdDisplay,
+  3, // LCD Line starting from 0
+  SENSOR_POLLING_INTERVAL
 );
 
 const basking = heatingControl(
-  [6],
+  [6], // Powerbar Socket to register
   baskingSensor,
   TARGET_BASKING_TEMP,
   "Basking",
   temperatureSensors,
-  lcd_display,
-  0,
-  5 * seconds
+  lcdDisplay,
+  0, // LCD Line starting from 0
+  SENSOR_POLLING_INTERVAL
 );
 
 const hide = heatingControl(
-  [4, 5],
+  [4, 5], // Powerbar Socket to register
   hideSensor,
   TARGET_HIDE_TEMPERATURE,
   "Warm Hide",
   temperatureSensors,
-  lcd_display,
-  1,
-  5 * seconds
+  lcdDisplay,
+  1, // LCD Line starting from 0
+  SENSOR_POLLING_INTERVAL
 );
 
 const cool = heatingControl(
@@ -58,9 +60,9 @@ const cool = heatingControl(
   null,
   "Cool Side",
   temperatureSensors,
-  lcd_display,
-  2,
-  5 * seconds
+  lcdDisplay,
+  2, // LCD Line starting from 0
+  SENSOR_POLLING_INTERVAL
 );
 
 module.exports = { basking, hide, cool, mister };
