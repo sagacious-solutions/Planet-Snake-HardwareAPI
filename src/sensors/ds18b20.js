885a8ec08917c69273bb419d440375db26ef326e
Matt Macdonald <mksmacdonald@gmail.com>
2021-09-19 18:15:27 -0700

    removed notes and unused

--- src/sensors/ds18b20.js
+++ src/sensors/ds18b20.js
@@ -1,26 +1,22 @@
+////////////////////////////////////////////////////////////////////////////
+// This module is used to fetch readings from all ds18b20 sensors on
+// the Raspberry Pi's one wire bus
+////////////////////////////////////////////////////////////////////////////
+
 const sensor = require("ds18b20-raspi");
 const time = require("../helpers/rtc");
 
 const seconds = 1000;
 
 module.exports = (intLength = 10 * seconds, dbSaveInterval = 5) => {
-  const FAKEDATA = [
-    { id: "28-0000058f5428", t: 33.63 },
-    { id: "28-0000058fc62a", t: 22.06 },
-    { id: "28-0214630eb5ff", t: 21.19 },
-    { id: "28-041464652fff", t: 38.56 },
-  ];
-
   const lastHr = [];
 
   const getLast15 = () => {
-    // console.log("getLast15 Ran ", lastHr.length - 15, " - ", lastHr.length);
     return lastHr.slice(lastHr.length - 15, lastHr.length);
   };
 
-  let count, lastReadData;
+  let lastReadData;
   let lastReadTime = time();
-  // Takes the time of last year, adds on the amount of minutes before a Db read
   let nextDBWrite = lastReadTime.time().minutes + dbSaveInterval;
 
   ///////////////////////////////////////////////////////////////////////////////////////////
@@ -43,9 +39,9 @@ module.exports = (intLength = 10 * seconds, dbSaveInterval = 5) => {
         nextDBWrite = 0 + dbSaveInterval;
       }
 
+      // THESE ARE PLACE HOLDERS. THE DATABASE WILL BE INTEGRATED SHORTLY
       console.log(`You should do database writes here.
       Find last hours readings below`);
-
       for (let reading of lastHr) {
         console.log(reading.time);
         console.log(reading.temps);
